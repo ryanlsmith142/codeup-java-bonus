bed42f73c84baf48ca1a4591cb37a32b5be83f62
Ryan Smith <ryan.l.smith142@gmail.com>
2019-09-18 16:17:52 -0500

    begin checking words logic

--- src/PigLatinCat.java
+++ src/PigLatinCat.java
@@ -7,6 +7,7 @@ public class PigLatinCat {
 
 
         // convert word to pig latin word (assume word doesn't begin with a vowel or end in ay)
+
 //        String cat = "cat"; // "atcay"
 //
 //        String firstLetter = cat.substring(0, 1);
@@ -16,6 +17,7 @@ public class PigLatinCat {
 //        String pigLatinWord = wordMinusFirstLetter + firstLetter + "ay";
 //
 //        System.out.println(pigLatinWord);
+
         System.out.println("Give me a sentence and I'll translate it to Pig Latin");
 
         String userInput = sc.nextLine();
@@ -24,15 +26,39 @@ public class PigLatinCat {
 
 
         for(String var: arrOfString) {
+
+            //COPIES FIRST LETTER OF STRING AND STORES IT
+
             String firstLetter = var.substring(0, 1);
+
+            //COPIES EVERYTHING IN STRING BEGINNING SECOND LETTER
 
             String wordMinusFirstLetter = var.substring(1, var.length());
 
+            //MAKES THE WORD INTO PIG LATIN
+
             String pigLatinString = wordMinusFirstLetter + firstLetter + "ay ";
 
-            System.out.print(pigLatinString);
+            //-----------------------------------------------------------------------------
+
+            //CHECK TO SEE IF WORDS HAVE PUNCTUATION
+
+            String[] pigLatinArray = pigLatinString.split(" ");
+
+            for(String nextVar: pigLatinArray) {
+                //Loop that checks each word and see if it has punctuation and then removes it
+                if(nextVar.indexOf("?") >= 1) {
+                    String correctedWord = nextVar.replace("?", "");
+                    nextVar += correctedWord;
+                    System.out.println(nextVar);
+                }
+
+
+            }
 
         }
+
+
 
 
 
