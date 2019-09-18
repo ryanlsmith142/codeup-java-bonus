f86c1b2be550a2cecbadd747f5db6cda8ad67fac
Ryan Smith <ryan.l.smith142@gmail.com>
2019-09-18 15:52:20 -0500

    add pig latin translator

--- src/PigLatinCat.java
+++ src/PigLatinCat.java
@@ -1,16 +1,38 @@
+import java.util.Scanner;
+
 public class PigLatinCat {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
         // convert word to pig latin word (assume word doesn't begin with a vowel or end in ay)
-        String cat = "cat"; // "atcay"
+//        String cat = "cat"; // "atcay"
+//
+//        String firstLetter = cat.substring(0, 1);
+//
+//        String wordMinusFirstLetter = cat.substring(1, cat.length());
+//
+//        String pigLatinWord = wordMinusFirstLetter + firstLetter + "ay";
+//
+//        System.out.println(pigLatinWord);
+        System.out.println("Give me a sentence and I'll translate it to Pig Latin");
 
-        String firstLetter = cat.substring(0, 1);
+        String userInput = sc.nextLine();
 
-        String wordMinusFirstLetter = cat.substring(1, cat.length());
+        String[] arrOfString = userInput.split(" ");
 
-        String pigLatinWord = wordMinusFirstLetter + firstLetter + "ay";
 
-        System.out.println(pigLatinWord);
+        for(String var: arrOfString) {
+            String firstLetter = var.substring(0, 1);
 
+            String wordMinusFirstLetter = var.substring(1, var.length());
+
+            String pigLatinString = wordMinusFirstLetter + firstLetter + "ay ";
+
+            System.out.print(pigLatinString);
+
+        }
 
 
 
