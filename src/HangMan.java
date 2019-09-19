5c5d4519a248d33e9832bf2bf5cd12430969c7e4
Ryan Smith <ryan.l.smith142@gmail.com>
2019-09-19 10:38:49 -0500

    start on hangman game

--- src/HangMan.java
+++ src/HangMan.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class HangMan {
     public static void main(String[] args) {
+        /*
+        STRING BONUS CHALLENGE
+        Create a command line Hangman game!
+        Create a Java class called Hangman. In the main method, you may find defining the following variables helpful (you decide which variables need an initialized starting value):
+        Scanner sc;
+        String wordSoFar;
+        String wordGuess;
+        String wordCopy = "";
+        String chooseGuessLetterOrWord;
+        String letterGuess;
+        int noOfRounds;
+        You decide how many guesses the user has before the game is over. You should start trying to make the game work with only three letter words.
+        Can you prevent the user from entering an invalid choice when asked to guess a letter or a word?
+        Can you make the console output look fancy?
+         */
+        Scanner sc = new Scanner(System.in);
+
+        String wordSoFar;
+
+        String wordGuess;
+
+        String wordCopy = "";
+
+        String chooseGuessLetterOrWord;
+
+        String letterGuess;
+
+        int noOfRounds;
+
+
+
+
+
+
 
     }
 }
